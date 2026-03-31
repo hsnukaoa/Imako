4764d6c7eb8fdc32711c703720e48bb4d783e4df
hsnukaoa <koudagawa@icloud.com>
2026-04-01 08:56:57 +0900

    Initial Commit

---
+++ Imako/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Imako
+//
+//  Created by 宇田川航太 on 2026/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Imako/ImakoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImakoApp.swift
+//  Imako
+//
+//  Created by 宇田川航太 on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ImakoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
